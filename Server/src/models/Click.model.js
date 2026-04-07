81a936f2bfed29bb40c39b595d2b19e1594a18b7
Akshay Ladne <Kumawata283@gmail.com>
2026-04-07 22:29:59 +0530

    feat: added a model for clicks

---
+++ Server/src/models/Click.model.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const clickSchema = new mongoose.Schema({
+    urlId:{type:mongoose.Schema.Types.ObjectId, ref:"Url", required:true},
+    shortcode:{type:String,  required: true},
+    device:{type:String},
+    ip:{type:String},
+    referrer:{type:String}
+},{timestamps:true})
+
+const Click = mongoose.model('Click', clickSchema);
+export default Click;
