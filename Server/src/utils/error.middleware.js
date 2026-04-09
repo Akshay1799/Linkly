779f6ee0b2b8a099ac1a0eeb1a58bc85fc40726c
Akshay Ladne <Kumawata283@gmail.com>
2026-04-09 12:40:17 +0530

    update: update global error handler middlware for environment-based error handling (dev vs prod)

--- Server/src/utils/error.middleware.js
+++ Server/src/utils/error.middleware.js
@@ -1,12 +1,30 @@
+import { config } from "../config/index.js";
+
 export const errorHandler = (err, req, res, next)=>{
     console.log(err);
 
     const statusCode = err.statusCode || 500;
     const status = err.status || "error";
 
-    res.status(statusCode).json({
-        status,
-        message: err.message || "Internal server error"
+    if(config.env === "development"){
+        return res.status(statusCode).json({
+            status,
+            message: err.message,
+            stack: err.stack
+        })
+    }
+
+    if(config.env === "production"){
+        if(err.statusCode){
+            return res.status(statusCode).json({
+                status,
+                message: err.message
+            })
+        }
+    }
+    res.status(500).json({
+        status: "error",
+        message: "Something went wrong"
     })
     
 }
