9b7e1fe9284ca139902df050fd906305fdf75e15
Akshay Ladne <Kumawata283@gmail.com>
2026-04-30 23:39:35 +0530

    feat(db): add optimized indexing with compound index for userId and createdAt; fix schema validation

--- Server/src/models/Url.model.js
+++ Server/src/models/Url.model.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
 const urlSchema = new mongoose.Schema({
-    userId:{type: mongoose.Schema.Types.ObjectId, ref: "User", req:true},
+    userId:{type: mongoose.Schema.Types.ObjectId, ref: "User", required:true, index: true},
     originalUrl: {type: String, required: true, trim: true},
     shortCode: {type: String, required: true, unique: true},
     customAlias: {type: String, trim: true},
     totalClicks: {type: Number, default:0}
-})
+},{timestamps: true})
+
+urlSchema.index({userId: 1, createdAt: -1})
 
 const Url = mongoose.model('Url', urlSchema);
 export default Url;
